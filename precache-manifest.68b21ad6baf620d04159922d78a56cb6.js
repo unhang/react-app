24b21740fa04d4db6057686106e1098460b28520
unknown <h.ung@goalist.co.jp>
2019-10-25 13:10:06 +0700

    Updates

---
+++ precache-manifest.68b21ad6baf620d04159922d78a56cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd599709954a2f62f24abbb0235ee20a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32761a44151dc77f101b",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a6696596144e8082034a",
+    "url": "/static/css/main.f5a72dc5.chunk.css"
+  },
+  {
+    "revision": "32761a44151dc77f101b",
+    "url": "/static/js/2.2f629d4e.chunk.js"
+  },
+  {
+    "revision": "a6696596144e8082034a",
+    "url": "/static/js/main.308796e9.chunk.js"
+  },
+  {
+    "revision": "c332e1bb67579b93fb1f",
+    "url": "/static/js/runtime-main.b75238ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
